05b4faaa3f5ced008613a848ae48fb66219913d7
nonzakiz <tawan.civil1995@gmail.com>
2020-01-26 22:00:58 +0700

    Update middleware from Auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,21 +46,19 @@ async (req,res)=>{
         user.password = await bcrypt.hash(password, salt)
 
         await user.save()
-        const payload ={
-            user:{
-                id:user.id
+          const payload ={
+                user:{
+                    id:user.id
 
+                }
             }
-        }
-        jwt.sign(payload,config.get('jwtSecret'),
-        {expiresIn:360000},
-        (err,token)=>{
-            if(err) throw err
-            res.json({token})
-        }
-        )
-
-        res.send('User register')
+            jwt.sign(payload,config.get('jwtSecret'),
+            {expiresIn:360000},
+            (err,token)=>{
+                if(err) throw err
+                res.json({token})
+            }
+            )
     }catch (err){
         console.log(err.message)
         res.status(500).send('Server error')
--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use('/api/profile',require('./routes/api/profile'))
 app.use('/api/posts',require('./routes/api/posts'))
 
 
-const PORT =process.env.PORT || 5003
+const PORT =process.env.PORT || 5005
 app.listen(PORT,() => console.log(`Server started on PORT ${PORT}`))
